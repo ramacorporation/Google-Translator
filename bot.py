59075691b3c54675fa1779e88f773aac7ba1a0fa
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-07-07 13:46:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "")
 app = Client(
         "ggt",
-        bot_token=TOKEN,api_hash=API_HASH,
-            api_id=API_ID
+        bot_token=TOKEN,api_id=API_ID,
+            api_hash=API_HASH
     )
 
 
